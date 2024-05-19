545e40fc5520567b278d76c1c783972f4937a2bf
unknown <rafaelaugusto.0112@gmail.com>
2024-05-19 11:17:47 -0300

    primeiro commit

--- main2.c
+++ main2.c
@@ -1,22 +1,42 @@
 #include<stdio.h>
 
 int main(){
-    int n, i;
+    int n, i, escala;
     float media, soma = 0;
 
     printf("Em quantos pontos voce vai querer medir a temperatura?");
     scanf("%d", &n);
     float ambiente[n];
 
+    printf("Escolha uma opçao:\n");
+    printf("1-Graus Celsius\n");
+    printf("2-Fahrenheit\n");
+    printf("3-Kelvin\n");
+    scanf("%d", &escala);
+
     for(i=0;i<n;i++){
-        printf("Digite a %d temperatura em graus: ", i+1);
+        printf("Digite a %d temperatura: ", i+1);
         scanf("%f", &ambiente[i]);
         soma += ambiente[i];
     }
 
     media = soma / n;
 
-    printf("A temperatura media do ambiente e de %.2f graus.", media);
+    switch(escala){
+        case 1:
+            printf("A temperatura media do ambiente e de %.2f graus.", media);
+            break;
+        case 2:
+            printf("A temperatura media do ambiente e de %.2f fahrenheit.", media);
+            break;
+        case 3:
+            printf("A temperatura media do ambiente e de %.2f kelvins.", media);
+            break;
+        default:
+            printf("Escala de termometrica invalida");
+            break;   
+    }
+
 
     return 0;
 }
